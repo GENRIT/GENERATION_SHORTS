6fd98b461d99ead1a69ed2543f0275ab51e66aef
GENRIT <OxyCloudMod@yandex.ru>
2024-09-17 17:47:50 +0500

    Update

---
+++ conf.py
@@ -0,0 +1,19 @@
+# conf.py
+
+# Токен для Telegram-бота
+API_TOKEN = '7407917160:AAGsC0Fo6tdiHzGXwWG-LKjvUdPEBL1Ipro'  # Поместите ваш токен сюда
+
+# Пути к директориям с видео и музыкой
+VIDEO_DIR = 'videos'  # Директория для видеофайлов
+MUSIC_DIR = 'music'   # Директория для музыкальных файлов
+
+# Путь к изображению логотипа
+IMAGE_PATH = 'image/logo.png'
+
+# Настройки для текста
+TEXT_FONT = 'Arial-Bold'  # Шрифт для текста
+TEXT_COLOR = 'white'      # Цвет текста
+TEXT_SIZE = 70            # Размер шрифта для текста
+
+# Громкость музыки в видео
+MUSIC_VOLUME = 0.1  # Громкость музыки (от 0 до 1)
